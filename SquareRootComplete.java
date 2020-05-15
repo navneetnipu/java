9aeff47168f2647c1ac986220ea078a54556c799
navneetnipu <40489986+navneetnipu@users.noreply.github.com>
2020-05-16 02:10:39 +0530

    Update SquareRootComplete.java
    complete documented program file
    end of project

--- SquareRootComplete.java
+++ SquareRootComplete.java
@@ -1,18 +1,24 @@
 import java.util.*;
 
-//String.valueOf(number_user_input)
-//Double.parseDouble(str)
-//Character.getNumericValue(c)
-//number_user_input_str.length()
-
+/*
+basic used typical functions
+String.valueOf(number_user_input)
+Double.parseDouble(str)
+Character.getNumericValue(c)
+number_user_input_str.length()
+*/
 
 class SquareRootComplete{
 
 	public static void main(String[] args) {
-		double Root1=0,Root2=0,Root=0,DecimalNum=0;
+		double Root1=0,Root2=0,Root=0,DecimalNum=0;// temporary variables to store the intermediate data
 		System.out.println("enter the number:");
 		Scanner s=new Scanner(System.in);
-		double UserInputNumber=s.nextDouble();
+		double UserInputNumber=s.nextDouble();// taking user input as double formatted number
+
+		/*
+			generating required output using diffeent methods
+		*/
 
 		DecimalNum=ReturnDenumDecimal(UserInputNumber);
 		int[] NumeratorArray=ReturnAsArray(UserInputNumber);
@@ -20,12 +26,16 @@ class SquareRootComplete{
 		int[] DenumeratorArray=ReturnAsArray(DecimalNum);
 		Root2=RootFunction(DenumeratorArray);
 		Root=(double)Root1/Root2;
-		System.out.println("the square root of the number is :"+Root);
+		System.out.println("the square root of the number is : "+Root);// printing the actual square root of the number
 	}
+
+	/*
+		method to generate the decimal part of the input number
+	*/
 
 	public static double ReturnDenumDecimal(double UserInputNumber){
 
-		int Length=0,Decimal=0;
+		int Length=0,Decimal=0;//temporary variables
 		double Denum=0;
 
 		String UserInputNumberStr = String.valueOf( UserInputNumber);
@@ -34,6 +44,10 @@ class SquareRootComplete{
 		long TempNum1=(long)UserInputNumber;
 		String TempNum1Str=String.valueOf(TempNum1);
 		int LengthOfInputDataWholePart=TempNum1Str.length();
+
+		/*
+			converting decimal part of the entered double number
+		*/
 
 		if (TempNum1==UserInputNumber) {
 			Length=LengthOfInputDataWholePart;
@@ -48,17 +62,19 @@ class SquareRootComplete{
 			Decimal=LengthOfInputCompleteData-LengthOfInputDataWholePart;
 		}
 		Denum=Math.pow(10,Decimal);
-		return Denum;
+		return Denum;// returning the actual decimal part of the entered number
 	}
 
+	/*
+		method to generate the array of the input number in usable form for generating square root of the number
+	*/
 	public static int[] ReturnAsArray(double UserInputNumber){
 
-		int i=0,Length=0,n=0,j=0;
-		
+		int i=0,Length=0,n=0,j=0;//temporary variables
 		int[] TempArr1;
 		int[] TempArr2;
 		int[] TempArr3;
-		int[] Array;
+		int[] Array;// actual usable array variable
 
 		String UserInputNumberStr = String.valueOf( UserInputNumber);
 		int LengthOfInputData=UserInputNumberStr.length();
@@ -67,13 +83,16 @@ class SquareRootComplete{
 		String TempNum1Str=String.valueOf(TempNum1);
 		int LengthOfInputDataWholePart=TempNum1Str.length();
 
+		/*
+			converting number into usable array
+		*/
+
 		if (TempNum1==UserInputNumber) {
 			Length=LengthOfInputDataWholePart;
 			TempArr1=new int[LengthOfInputDataWholePart];
 			for(i=0;i<(LengthOfInputDataWholePart);i++){
 				TempArr1[i]=Character.getNumericValue(TempNum1Str.charAt(i));
 			}	
-
 		}
 		else{
 			double TempNum2=Math.pow(10,LengthOfInputData-LengthOfInputDataWholePart-1)*UserInputNumber;
@@ -102,15 +121,23 @@ class SquareRootComplete{
 		}
 		Array=new int[n];
 		Array=TempArr3;
-		return Array;
+		return Array;//returning actual usable array
 	}
 
+	/*
+		method to generate the square root of any given valid double number by continuous division method
+	*/
 
 	public static double RootFunction(int[] Num){
 
-		int x=0,Remainder=0,R=0,Sum=0,Divident=0,k=1,i=0;
+		int x=0,Remainder=0,R=0,Sum=0,Divident=0,k=1,i=0;// temporary variables
 		double Root=0,count=0,FinalRoot=0;
-		long DecRoot=0;
+		long DecRoot=0;//dcimal part root
+
+		/*
+			square root my long division method
+		*/
+
 		for (i=0;i<(Num.length);i++) {
 			Divident=(Remainder*100)+Num[i];
 				if (((Sum*10)+1)<=Divident) {
@@ -134,6 +161,10 @@ class SquareRootComplete{
 						Sum=Sum*10;
 					}
 		}
+
+		/*
+			generating square root after decimal point
+		*/
 
 		if(Remainder==0) {
 			FinalRoot=Root;
@@ -178,9 +209,10 @@ class SquareRootComplete{
 	    		}
 	    		String t= String.valueOf(DecRoot);
 	    		int p=t.length();
-	    		FinalRoot=(double)(((double)DecRoot)/(Math.pow(10,p)))+Root;
-
+	    		FinalRoot=(double)(((double)DecRoot)/(Math.pow(10,p)))+Root;//final square root
 		}
-		return FinalRoot;
+		return FinalRoot;//returning final root value 
 	}
 }
+
+// END OF PROGRAM
